f727febbf7c0bc5fe50d9b762ababbd6b5bcd07b
Lucas Parry <lucas@envato.com>
2013-12-29 15:42:23 +0000

    Fix broken ass tag gen plugin

--- plugins/tag_gen.rb
+++ plugins/tag_gen.rb
@@ -19,15 +19,12 @@ module Jekyll
       if site.layouts.key? 'tag_index'
         dir = site.config['tag_dir'] || 'tag'
         site.tags.keys.each do |tag|
-          write_tag_index(site, File.join(dir, tag), tag)
+          add_to_site_pages(site, File.join(dir, tag), tag)
         end
       end
     end
-    def write_tag_index(site, dir, tag)
-      index = TagIndex.new(site, site.source, dir, tag)
-      index.render(site.layouts, site.site_payload)
-      index.write(site.dest)
-      site.pages << index
+    def add_to_site_pages(site, dir, tag)
+      site.pages << TagIndex.new(site, site.source, dir, tag)
     end
   end
 end
